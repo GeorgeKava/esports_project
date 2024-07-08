1da75ba27fd319cbcde92b8b21be5d3109d80082
George <georgesash@outlook.com>
2024-07-08 08:46:33 -0500

    Final touch

--- server.py
+++ server.py
@@ -3,10 +3,26 @@ from waitress import serve
 
 app = Flask(__name__)
 
+#read file
+def read_file():
+    results = []
+    with open('data/person.txt', 'r') as file:
+        for line in file:
+            person = line.strip().split(',')
+            results.append({
+                'id': person[0],
+                'firstname': person[1],
+                'lastname': person[2],
+                'username': person[3],
+                'country': person[4]
+            })
+    return results
+
 #write person
-def write_to_file(data):
-    with open('data/person.txt', 'a') as file:
-        file.write(data +'\n')
+def write_to_file(results):
+    with open('data/person.txt', 'w') as file:
+        for person in results:
+            file.write(f"{person['id']},{person['firstname']},{person['lastname']},{person['username']},{person['country']}\n")
         
 # Home page
 @app.route('/')
@@ -22,10 +38,16 @@ def add_person():
         username = request.form['username']
         country = request.form['country']
         
-        data = f"{firstname}, {lastname}, {username}, {country}"
-        
-        write_to_file(data)
-        
+        results = read_file()
+        id = str(len(results) + 1)
+        results.append({
+            'id': id,
+            'firstname': firstname,
+            'lastname': lastname,
+            'username': username,
+            'country': country
+        })
+        write_to_file(results)
         return redirect(url_for('index'))
     
     return render_template('add_person.html')
@@ -35,18 +57,35 @@ def add_person():
 def search_person():
     if request.method == 'POST':
         search_term = request.form['search_term'].strip().lower()
-        
-        results = []
-        with open('data/person.txt', 'r') as file:
-            for line in file:
-                if search_term in line.lower():
-                    results.append(line.strip().split(','))
-                    
-        return render_template('search_person.html', results=results, search_term=search_term)
-    
+        results = read_file()
+        search_results = [person for person in results if search_term in person['firstname'].lower() or
+                                                            search_term in person['lastname'].lower() or
+                                                            search_term in person['username'].lower() or 
+                                                            search_term in person['country'].lower()]
+        return render_template('search_person.html', results=search_results, search_term=search_term)
     return render_template('search_person.html', results=None, search_term=None)
 
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update_person(id):
+    results = read_file()
+    persons = next((person for person in results if person['id'] == str(id)), None)
+    if persons:
+        if request.method == 'POST':
+            persons['firstname'] = request.form['firstname']
+            persons['lastname'] = request.form['lastname']
+            persons['username'] = request.form['username']
+            persons['country'] = request.form['country']
+            write_to_file(results)
+            return redirect(url_for('index'))
+        return render_template('update_person.html', persons = persons)
+    return 'Person not found', 404
 
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+def delete_person(id):
+    results = read_file()
+    results = [person for person in results if person['id'] != str(id)]
+    write_to_file(results)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
